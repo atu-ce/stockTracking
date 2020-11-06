27526498dd9ceb25adff6018e1a1f344ebeb8e06
atu46100 <atuce@gmail.com>
2020-11-06 12:19:59 +0300

    supplier component'te tasarim düzeltildi, service olusturuldu.

--- src/app/supplier/supplier.component.ts
+++ src/app/supplier/supplier.component.ts
@@ -1,22 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Supplier } from './supplier-model';
+import { SupplierService } from './supplier.service';
 
 @Component({
   selector: 'app-supplier',
   templateUrl: './supplier.component.html',
-  styleUrls: ['./supplier.component.css']
+  styleUrls: ['./supplier.component.css'],
+  providers: [SupplierService]
 })
 export class SupplierComponent implements OnInit {
 
-  constructor() { }
+  getAllSupplierObject: Observable<any>;
+
+  constructor(private supplierService: SupplierService, private router: Router) { }
 
   ngOnInit(): void {
+    this.getData();
   }
 
-  model_supplier = new Supplier();
-
-  getSupplier() {
-    return this.model_supplier.supplier_name;
+  getData(): void {
+    this.supplierService.getAllSupplierLists().subscribe(
+      res => {
+        this.getAllSupplierObject = res;
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
+  navigateToStockId(id) {
+    this.router.navigate(['/supplier/update', id]);
+ }
 }
---
+++ src/app/supplier/supplier.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupplierService {
+
+  baseUrl = 'http://127.0.0.1:5000/api/';
+  endPoint = 'suppliers/'
+  httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  constructor(private http: HttpClient) { }
+
+  getAllSupplierLists(): Observable<any> {
+    return this.http.get(`${this.baseUrl}${this.endPoint}`, { headers: this.httpHeaders });
+  }
+
+  putSupplier(supplierData): Observable<any> {
+    return this.http.put(`${this.baseUrl}${this.endPoint}${supplierData.id}`, JSON.stringify(supplierData), { headers: this.httpHeaders });
+  }
+}

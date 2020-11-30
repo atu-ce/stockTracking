22d1312a31a79d484ac550bf725758367e264426
atu46100 <atuce@gmail.com>
2020-11-30 09:15:22 +0300

    modalda yazilacak metin sadelestirildi.

--- src/app/stock/update/update.component.ts
+++ src/app/stock/update/update.component.ts
@@ -19,9 +19,6 @@ export class UpdateComponent implements OnInit {
   currencyObject: any;
   basicUnitObject: any;
   supplierObject: any;
-  isChanged: boolean = false;
-  changedObjects: Array<any> = [];
-  getAndUnchangedStockLists: any;
 
   constructor(
     private stockService: StockService,
@@ -36,7 +33,6 @@ export class UpdateComponent implements OnInit {
       this.stockService.getStockById(stockId).subscribe(
         res => {
           this.stockObject = res;
-          this.getAndUnchangedStockLists=this.stockObject;
         },
         err => {
           throw err;
@@ -98,8 +94,6 @@ export class UpdateComponent implements OnInit {
 
   
   onSubmit(f: NgForm) {
-    console.log(this.getAndUnchangedStockLists);
-    console.log(this.stockObject);
     
     if (f.valid) {
       // Django'daki veri tabani listesiyle gonderilecek listenin ayni olmasi icin eklenen degiskenler.
@@ -123,36 +117,6 @@ export class UpdateComponent implements OnInit {
 
       f.value.id = this.stockObject.id;
 
-      // Modalda yazilacak metin icin gerekli kodlar.
-      if(f.value.total_amount != this.getAndUnchangedStockLists.total_amount){
-        this.isChanged = true;
-        this.changedObjects.push(`Total Amount değeri: ${this.getAndUnchangedStockLists.total_amount} iken, ${f.value.total_amount} yapıldı.`); 
-      }
-      if(f.value.is_active != this.getAndUnchangedStockLists.is_active){
-        this.isChanged = true;
-        this.changedObjects.push(`is Active değeri: ${this.getAndUnchangedStockLists.is_active} iken, ${f.value.is_active} yapıldı.`); 
-      }
-      if(f.value.stock_name != this.getAndUnchangedStockLists.stock_name){
-        this.isChanged = true;
-        this.changedObjects.push(`Stock Name değeri: ${this.getAndUnchangedStockLists.stock_name} iken, ${f.value.stock_name} yapıldı.`); 
-      }
-      if(f.value.unit_price != this.getAndUnchangedStockLists.unit_price){
-        this.isChanged = true;
-        this.changedObjects.push(`Unit Price değeri: ${this.getAndUnchangedStockLists.unit_price} iken, ${f.value.unit_price} yapıldı.`); 
-      }
-      if(f.value.currency_code != this.getAndUnchangedStockLists.currency_code){
-        this.isChanged = true;
-        this.changedObjects.push(`Currency değeri: ${this.getAndUnchangedStockLists.currency_code} iken, ${f.value.currency_code} yapıldı.`); 
-      }
-      if(f.value.basic_unit_code != this.getAndUnchangedStockLists.basic_unit_code){
-        this.isChanged = true;
-        this.changedObjects.push(`Basic Unit değeri: ${this.getAndUnchangedStockLists.basic_unit_code} iken, ${f.value.basic_unit_code} yapıldı.`); 
-      }
-      if(f.value.supplier_name != this.getAndUnchangedStockLists.supplier_name){
-        this.isChanged = true;
-        this.changedObjects.push(`Supplier Name değeri: ${this.getAndUnchangedStockLists.supplier_name} iken, ${f.value.supplier_name} yapıldı.`); 
-      }
-      
       this.stockService.putStock(f.value).subscribe(
         res => { },
         err => {
